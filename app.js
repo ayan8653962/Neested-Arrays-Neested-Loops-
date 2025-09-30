1334cbdc5bd34e97ec30ebd8f8743a5593fc388e
ayan8653962 <ayan8653962@gmail.com>
2025-09-30 23:46:18 +0500

    practice

---
+++ app.js
@@ -0,0 +1,13 @@
+// question 1
+for (var i = 0; i < 3; i++) {   // rows
+    var row = "";
+    for (var j = 0; j < 3; j++) {   // columns
+        row += "* ";
+    }
+    document.write(row + "<br>");
+}
+
+
+
+
+
