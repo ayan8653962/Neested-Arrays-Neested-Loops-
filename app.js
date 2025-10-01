c28acddebea85c5d9add6428d4d3330d5b5ee2cd
ayan8653962 <ayan8653962@gmail.com>
2025-10-02 01:53:54 +0500

    practice

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ for (var i = 0; i < 3; i++) {   // rows
     }
     document.write(row + "<br>");
 }
+
 // question 2
 for(var i=1; i<5; i++){
   for(var j=1; j<5; j++){
@@ -15,13 +16,29 @@ document.write("<br>")
 }
 
 // question 3
+var divide = "" ;
+for(j=1; j<8; j++){
+  document.write(divide += "* ")
+  document.write("<br> ")  
+}
+// document.write("<br>")
+// question 4
 for(var i=1; i<6; i++){
     for(j=1; j<6; j++){
         document.write(j , " ")
     }
   document.write("<br>")
 }
-
+ document.write("<br>")
+// question 5
+for(var i=1; i<=5 ; i++){
+  for(var j=1; j<i; j++){
+   document.write(j + " ")
+  }
+  document.write("<br>")
+}
+ document.write("<br>")
+// question 6
 
 
 
