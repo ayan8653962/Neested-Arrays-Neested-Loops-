a98a8222ec9bfeb2a134c01354970ac75738734f
ayan8653962 <ayan8653962@gmail.com>
2025-10-07 01:45:54 +0500

    prctice

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ for(var i=1; i<6; i++){
     }
   document.write("<br>")
 }
-//  document.write("<br>")
+ document.write("<br>")
 // question 5
 for(var i=1; i<=5 ; i++){
   for(var j=1; j<i; j++){
@@ -150,8 +150,81 @@ for(var i=0; i<array.length; i++){
   }
 }
 document.writeln("Smollest number of an arry " + min)
-
-
-
-
-
+document.writeln("<br>")
+// question 15
+var count =0;
+var array = [
+  [1,2,3,4,5,6],
+  [7,8,9,10,11,12],
+  [13,14,15,16,17,18]
+];
+for(var i=0; i<array.length; i++){
+  for(var j=0; j<array[i].length; j++){
+    if(array[i][j] % 2 ===0 ){
+      count++
+    }
+  }
+}
+document.writeln("Total even number in array " + count)
+document.writeln("<br>")
+// question 16
+var odd = 0; 
+var array = [
+  [1,2,3,4,5,6],
+  [7,8,9,10,11,12],
+  [13,14,15,16,17,18]
+];
+for(var i=0; i<array.length; i++){
+  for(var j=0; j<array[i].length; j++){
+    if(array[i][j] % 2 !== 0 ){
+      odd++
+    }
+  }
+}
+document.writeln("Total odd number in array " + odd)
+document.writeln("<br>")
+  // question 17
+  var negative = 0;
+  var array = [
+    [-7,-6,-5,-4,-3,-2,-1,0],
+    [1,2,3,4,5,6],
+    [7,8,9,10,11,12]
+  ];
+  for(var i=0; i<array.length; i++){
+    for(var j=0; j<array[i].length; j++){
+      if(array[i][j] < 0){
+     array[i][j] =  negative 
+      }
+    }
+  }
+  document.writeln("Reaplace negative number from zero 0 ")
+  document.writeln("<br>")
+  // question 18
+  var diviby5 = 0;
+  var array = [
+    [-7,-6,-5,-4,-3,-2,-1,0],
+    [1,2,3,4,5,6],
+    [7,8,9,10,11,12]
+  ];
+  for(var i=0; i<array.length; i++){
+    for(var j=0; j<array[i].length; j++){
+      if(array[i][j] % 5 === 0 ){
+        diviby5++
+      }
+    }
+  }
+  document.writeln("Total divide by 5 = " + diviby5)
+  document.writeln("<br>")
+  // question 19
+ var array =[
+  [1,2,3,4,5],
+  [6,7,8,9,10],
+  [11,12,13,14,15]
+ ];
+ for(var i=0; i<array.length; i++){
+  var sum =0;
+  for(var j=0; j<array[i].length; j++){
+    sum +=array[i][j]
+  }
+  document.writeln("Row " + (i + 1) + " ka sum " + sum + "<br>")
+ }
